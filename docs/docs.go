e4bcaab990e4431b1528eb1e4d8ceac53a871d69
rifat977 <itscrifat5147@gmail.com>
2025-07-26 11:15:40 +0600

    change url

--- docs/docs.go
+++ docs/docs.go
@@ -821,7 +821,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "2.0",
-	Host:             "34.131.155.11",
+	Host:             "34.102.83.157",
 	BasePath:         "/",
 	Schemes:          []string{"http", "https"},
 	Title:            "High Performance Go API with Assets Management",
